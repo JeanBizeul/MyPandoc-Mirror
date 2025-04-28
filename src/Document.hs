cece05b9a05a0c7d184e24b21e63405429ce8234
JeanBizeul <jean.bizeul@epitech.eu>
2025-04-28 13:49:40 +0200

    ✨ Add Document data type

---
+++ src/Document.hs
@@ -0,0 +1,59 @@
+{-
+-- EPITECH PROJECT, 2025
+-- MyPandoc-Mirror
+-- File description:
+-- Document
+-}
+
+module Document where
+
+data Document = Document {
+    header :: Header,
+    body :: [Content]
+}
+
+newtype Date = Date String
+
+data Header = Header {
+    title :: String,
+    author :: Maybe String,
+    date :: Maybe Date
+}
+
+data FormatType = Italic | Bold | Code
+
+data Link = Link {
+    label :: String,
+    link :: String
+}
+
+data Image = Image {
+    altText :: String,
+    url :: String
+}
+
+newtype Paragraph = Paragraph [Content]
+
+data Section = Section {
+    sectionTitle :: Maybe String,
+    sectionBody :: [Content]
+}
+
+data CodeBlock = CodeBlock {
+    code :: String,
+    language :: Maybe String
+}
+
+newtype Item = Item [Content]
+
+newtype List = List [Item]
+
+data Content =
+    Text String
+    | Formatted FormatType Content
+    | LinkContent Link
+    | ImageContent Image
+    | ParagraphContent Paragraph
+    | SectionContent Section
+    | CodeBlockContent CodeBlock
+    | ListContent List

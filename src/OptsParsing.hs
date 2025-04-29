de923d2936d5750584bb0d0d11d0ff3c1c1af419
JeanBizeul <jean.bizeul@epitech.eu>
2025-04-29 19:14:57 +0200

    ♻️ Refactor file format handling in options parser to use FileFormat type

--- src/OptsParsing.hs
+++ src/OptsParsing.hs
@@ -7,13 +7,27 @@
 
 module OptsParsing (Options(..), opts) where
 
-import Options.Applicative (Parser, strOption, long, short, metavar, help, optional, execParser, info, fullDesc)
+import Options.Applicative (Parser, strOption, long, short, metavar, help, optional, execParser, info, fullDesc, eitherReader, option, ReadM)
+
+data FileFormat = Markdown | XML | JSON
+  deriving (Show, Eq)
+
+stringToFileFormat :: String -> Maybe FileFormat
+stringToFileFormat "markdown" = Just Markdown
+stringToFileFormat "xml"     = Just XML
+stringToFileFormat "json"    = Just JSON
+stringToFileFormat _         = Nothing
+
+fileFormatReader :: ReadM FileFormat
+fileFormatReader = eitherReader $ \arg -> case stringToFileFormat arg of
+        Just format -> Right format
+        Nothing     -> Left $ "Invalid file format: " ++ arg
 
 data Options = Options {
     inputFilePath :: String,
-    outputFileFormat :: String,
+    outputFileFormat :: FileFormat,
     outputFilePath :: Maybe String,
-    inputFileFormat :: Maybe String
+    inputFileFormat :: Maybe FileFormat
 } deriving (Show)
 
 opts :: Parser Options
@@ -23,17 +37,20 @@ opts = Options
         <> short 'i'
         <> metavar "FILEPATH"
         <> help "Path to the input file" )
-    <*> strOption
+
+    <*> option fileFormatReader
         ( long "output-format"
         <> short 'f'
         <> metavar "FORMAT"
         <> help "Output file format (markdown, XML or JSON)" )
+
     <*> optional (strOption
         ( long "output"
         <> short 'o'
         <> metavar "FILEPATH"
         <> help "Path to the output file" ))
-    <*> optional (strOption
+
+    <*> optional (option fileFormatReader
         ( long "input-format"
         <> short 'e'
         <> metavar "FORMAT"

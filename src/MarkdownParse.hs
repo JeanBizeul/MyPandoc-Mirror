37f9583eb87aa12eedf23c3ee2f6cf4d7485fa95
dxgtnnn <davy.gatineau@epitech.eu>
2025-05-04 15:27:44 +0200

    🔨 Updated and fixed Markdown parser - is working quite well

--- src/MarkdownParse.hs
+++ src/MarkdownParse.hs
@@ -1,24 +1,34 @@
 {-
 -- EPITECH PROJECT, 2025
--- MyPandoc-Mirror
+-- mirror
 -- File description:
--- MarkdownParser
+-- MarkdownParse
 -}
 
 module MarkdownParse (parseMarkdown) where
 
 import Document
-import Data.Maybe (fromMaybe, mapMaybe)
-import Data.List (stripPrefix, isPrefixOf, isSuffixOf, isInfixOf)
+import Data.Maybe (fromMaybe, mapMaybe, isJust)
+import Data.List (stripPrefix, isPrefixOf, isSuffixOf, isInfixOf, tails)
 import Data.Char (isSpace)
 
-parseMarkdown :: String -> Document
+breakOn :: Eq a => [a] -> [a] -> ([a], [a])
+breakOn delim str = go str
+  where
+    go [] = ([], [])
+    go s@(x:xs)
+      | delim `isPrefixOf` s = ([], drop (length delim) s)
+      | otherwise = let (first, rest) = go xs in (x:first, rest)
+
+parseMarkdown :: String -> Maybe Document
 parseMarkdown input =
     let ls = lines input
         (headerLines, contentLines) = splitHeader ls
         hdr = parseHeader headerLines
         body = parseBody contentLines
-    in Document hdr body
+    in if isValidHeader hdr
+        then Just (Document hdr body)
+        else Nothing
 
 splitHeader :: [String] -> ([String], [String])
 splitHeader ("---":xs) =
@@ -40,33 +50,66 @@ parseKV line =
         (key, ':':val) -> Just (trim key, trim val)
         _              -> Nothing
 
+isValidHeader :: Header -> Bool
+isValidHeader (Header t _ _) = not (null t)
+
 parseBody :: [String] -> [Content]
 parseBody [] = []
 parseBody (l:ls)
     | all (== '-') l || null l = parseBody ls
-    | take 3 l == "```" =
-        let (code, rest) = break (== "```") ls
-        in CodeBlockContent (CodeBlock (unlines code) Nothing) : parseBody (drop 1 rest)
-    | take 2 l == "##" =
-        let t = trim $ drop 2 l
-        in SectionContent (Section (Just t) []) : parseBody ls
-    | take 2 l == "- " =
-        let (items, rest) = span (\l -> take 2 l == "- ") (l:ls)
-        in ListContent (List (map parseItem items)) : parseBody rest
-    | otherwise =
-        ParagraphContent (Paragraph [parseInlines l]) : parseBody ls
+    | take 3 l == "```" = parseCodeBlock ls
+    | take 2 l == "##" = parseSection l ls
+    | take 2 l == "- " = parseList l ls
+    | otherwise = parseParagraph l ls
+
+parseCodeBlock :: [String] -> [Content]
+parseCodeBlock ls =
+    let (code, rest) = break (== "```") ls
+    in CodeBlockContent 
+    (CodeBlock (map Text code) Nothing) : parseBody (drop 1 rest)
+
+parseSection :: String -> [String] -> [Content]
+parseSection l ls =
+    let t = trim $ drop 2 l
+        title = Title t 2
+        section = Section (Just title) []
+    in SectionContent section : parseBody ls
+
+parseList :: String -> [String] -> [Content]
+parseList l ls =
+    let (items, rest) = span (\x -> take 2 x == "- ") (l:ls)
+    in ListContent (List (map parseItem items)) : parseBody rest
+
+parseParagraph :: String -> [String] -> [Content]
+parseParagraph l ls = 
+    ParagraphContent (Paragraph [parseInlines l]) : parseBody ls
 
 parseItem :: String -> Item
 parseItem l = Item [parseInlines (drop 2 l)]
 
 parseInlines :: String -> Content
 parseInlines s
-    | "**" `isWrappedIn` s = Formatted Bold (Text $ unwrap "**" s)
-    | "*"  `isWrappedIn` s = Formatted Italic (Text $ unwrap "*" s)
-    | "`"  `isWrappedIn` s = Formatted Code (Text $ unwrap "`" s)
+    | "**" `isWrappedIn` s = 
+        Formatted (Bold [Text $ unwrap "**" s]) (Text $ unwrap "**" s)
+    | "*"  `isWrappedIn` s = 
+        Formatted (Italic [Text $ unwrap "*" s]) (Text $ unwrap "*" s)
+    | "`"  `isWrappedIn` s = 
+        Formatted (Code (unwrap "`" s)) (Text $ unwrap "`" s)
     | "[" `isInfixOf` s && "](" `isInfixOf` s = parseLink s
     | "![" `isInfixOf` s && "](" `isInfixOf` s = parseImage s
     | otherwise = Text s
+
+parseLink :: String -> Content
+parseLink s =
+    case span (/= ']') (drop 1 s) of
+        (lbl, ']':'(':url) -> LinkContent (Link [Text lbl] (init url))
+        _ -> Text s
+
+parseImage :: String -> Content
+parseImage s =
+    case span (/= ']') (drop 2 s) of
+        (alt, ']':'(':url) -> ImageContent (Image alt (init url))
+        _ -> Text s
 
 isWrappedIn :: String -> String -> Bool
 isWrappedIn delim s = delim `isPrefixOf` s && delim `isSuffixOf` s
@@ -79,15 +122,3 @@ stripSuffix suf xs = reverse <$> stripPrefix (reverse suf) (reverse xs)
 
 trim :: String -> String
 trim = f . f where f = reverse . dropWhile isSpace
-
-parseLink :: String -> Content
-parseLink s =
-    case span (/= ']') (drop 1 s) of
-        (label, ']':'(':url) -> LinkContent (Link label (init url))
-        _                    -> Text s
-
-parseImage :: String -> Content
-parseImage s =
-    case span (/= ']') (drop 2 s) of
-        (alt, ']':'(':url) -> ImageContent (Image alt (init url))
-        _                  -> Text s
